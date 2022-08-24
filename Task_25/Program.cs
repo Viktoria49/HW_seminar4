e9732d8814c79e764ddb69ec40f81a97658ef10f
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-24 11:20:26 +0700

    Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и
+//  возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write ("Введите число для возведения в степень: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите натуральное число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int numA, int numB)
+{
+int result = 1;
+for(int i=1; i <= numB; i++)
+{
+result = result * numA;
+}
+return result;
+}
+int exponentiation = Exponentiation(numA, numB);
+Console.Write("Ответ: " + exponentiation);
