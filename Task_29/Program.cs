3756e52b01a21a88c0918ba5bf3b598433c21862
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-24 11:31:58 +0700

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и
+//  выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.WriteLine("Введите размер массива: ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+
+int[] RandArray(int len)
+{
+    int[] arrays = new int[len];
+    for (int i = 0; i < arrays.Length; i++)
+    {
+        arrays[i] = new Random().Next(0, 100);
+    }
+    return arrays;
+}
+
+int[] res = RandArray(lenght);
+
+void PrintArray(int[] res1)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < res1.Length; i++)
+    {
+        Console.Write($"{res1[i]}, ");
+    }
+    Console.Write($"]");
+}
+
+PrintArray(res);
